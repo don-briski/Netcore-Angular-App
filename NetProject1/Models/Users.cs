814fa68a86a646713fb460a7b84a2f0df05bae58
don-briski01~ <abdulahiemoabino2@gmail.com>
2023-07-17 17:41:22 +0100

    creating my dotNet first app

---
+++ NetProject1/Models/Users.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NetProject1.Models
+{
+    public class User
+    {
+        [Key]
+
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Token { get; set; }
+        public bool Role { get; set; }
+        public int Email { get; set; }
+
+
+
+
+
+    }
+}
